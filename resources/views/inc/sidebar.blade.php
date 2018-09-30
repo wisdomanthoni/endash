5b1465e43ad64c44859fccf66e0df26a2f0f5099
Wisdom Anthony <wizitendo10@gmail.com>
2018-09-30 10:49:55 +0100

    fix some stuffs

---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,40 @@
+<div class="sidebar-wrapper">
+    <div class="logo">
+        <a href="#" class="simple-text">
+            ENDASH
+        </a>
+    </div>
+
+    <ul class="nav">
+        <li class="active">
+            <a href="{{ url('/') }}">
+                <i class="ti-panel"></i>
+                <p>Dashboard</p>
+            </a>
+        </li>
+        <li>
+            <a href="{{ url('/players') }}">
+                <i class="ti-user"></i>
+                <p>Players Profile</p>
+            </a>
+        </li>
+        <li>
+            <a href="{{ url('/articles') }}">
+                <i class="ti-notepad"></i>
+                <p>Write Articles</p>
+            </a>
+        </li>
+        <li>
+            <a href="table.html">
+                <i class="ti-view-list-alt"></i>
+                <p>Match Fixing</p>
+            </a>
+        </li>
+        <li>
+            <a href="notifications.html">
+                <i class="ti-bell"></i>
+                <p>Notifications</p>
+            </a>
+        </li>
+    </ul>
+</div>
