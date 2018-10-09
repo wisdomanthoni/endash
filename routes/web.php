f8df4b0b3228a3e267cb59e6597a0a4cc0c292e6
Wisdom Anthony <wizitendo10@gmail.com>
2018-10-09 20:04:34 +0100

    added some controllers

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('matches', function () {
+    Route::resource('/seasons', 'SeasonController');
+    Route::resource('/competitions', 'CompController');
+    Route::resource('/clubs', 'ClubController');
 
 }); 
